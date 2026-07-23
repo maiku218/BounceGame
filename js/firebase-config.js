7dd3b9b4dd3208c202fcedbf18e417dffbd5cea9
maiku0627 <mikeangelodejuan0627@gmail.com>
2026-07-24 00:43:30 +0800

    Summary

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,11 @@
 var firebaseConfig = {
-	apiKey: "YOUR_API_KEY",
-	authDomain: "YOUR_PROJECT.firebaseapp.com",
-	projectId: "YOUR_PROJECT_ID",
-	storageBucket: "YOUR_PROJECT.appspot.com",
-	messagingSenderId: "YOUR_SENDER_ID",
-	appId: "YOUR_APP_ID"
+	apiKey: "AIzaSyA59VlyQlg7N50mGn14oURh3THGgkWxGS4",
+	authDomain: "bouncegame-fc983.firebaseapp.com",
+	projectId: "bouncegame-fc983",
+	storageBucket: "bouncegame-fc983.firebasestorage.app",
+	messagingSenderId: "439377809691",
+	appId: "1:439377809691:web:5384655abe2c0dc59f83be",
+	measurementId: "G-8ECM457FX5"
 };
 firebase.initializeApp(firebaseConfig);
 const firebaseAuth = firebase.auth();
