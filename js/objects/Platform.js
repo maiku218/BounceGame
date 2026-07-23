e83c3cc4454ee4cb836dad88b859cb2f9550cac2
maiku0627 <mikeangelodejuan0627@gmail.com>
2026-07-24 00:51:41 +0800

    Simplify levels to plain 2D blocks

--- js/objects/Platform.js
+++ js/objects/Platform.js
@@ -21,46 +21,20 @@ class Platform extends Phaser.GameObjects.Container {
 		const x = this.x - w / 2;
 		const y = this.y - h / 2;
 
-		let topColor, bottomColor, borderColor;
+		const colors = {
+			normal: 0xd2691e,
+			ice: 0x6ec6ff,
+			sticky: 0x8b6914,
+			breakable: 0x883333,
+			moving: 0xbb8844
+		};
 
-		if (this.type === 'ice') {
-			topColor = 0xaeeeff;
-			bottomColor = 0x6ec6ff;
-			borderColor = 0x3388bb;
-		} else if (this.type === 'sticky') {
-			topColor = 0xd2b48c;
-			bottomColor = 0x8b6914;
-			borderColor = 0x5c4400;
-		} else if (this.type === 'breakable') {
-			topColor = 0xcc7777;
-			bottomColor = 0x883333;
-			borderColor = 0x552222;
-		} else if (this.type === 'moving') {
-			topColor = 0xddaa88;
-			bottomColor = 0xbb8844;
-			borderColor = 0x885522;
-		} else {
-			topColor = CONFIG.COLORS.BLOCK_ORANGE;
-			bottomColor = CONFIG.COLORS.BLOCK_BROWN;
-			borderColor = 0x000000;
-		}
+		const color = colors[this.type] || colors.normal;
 
-		this.graphics.fillStyle(bottomColor, 1);
-		this.graphics.fillRoundedRect(x, y + 3, w, h - 3, 4);
-		this.graphics.fillStyle(topColor, 1);
-		this.graphics.fillRoundedRect(x, y, w, h - 3, 4);
-
-		this.graphics.lineStyle(1, 0xffffff, 0.3);
-		this.graphics.strokeRoundedRect(x + 1, y + 1, w - 2, h - 5, 4);
-
-		this.graphics.fillStyle(0xffffff, 0.15);
-		this.graphics.fillRect(x + 4, y + 4, w - 8, 4);
-
-		this.graphics.lineStyle(2, borderColor, 0.8);
-		this.graphics.strokeRoundedRect(x, y, w, h - 3, 4);
-
-		this.graphics.fillStyle(bottomColor, 0.8);
-		this.graphics.fillRect(x, y + h - 5, w, 5);
+		this.graphics.fillStyle(color, 1);
+		this.graphics.fillRect(x, y, w, h);
+		this.graphics.lineStyle(2, 0x000000, 1);
+		this.graphics.strokeRect(x, y, w, h);
 	}
 }
 
@@ -91,12 +65,10 @@ class MovingPlatform extends Phaser.GameObjects.Container {
 		const x = this.x - w / 2;
 		const y = this.y - h / 2;
 
-		this.graphics.fillStyle(0xd2b48c, 1);
-		this.graphics.fillRoundedRect(x, y + 3, w, h - 3, 4);
-		this.graphics.fillStyle(0xf0dcc0, 1);
-		this.graphics.fillRoundedRect(x, y, w, h - 3, 4);
-		this.graphics.lineStyle(2, 0x885522, 0.8);
-		this.graphics.strokeRoundedRect(x, y, w, h - 3, 4);
+		this.graphics.fillStyle(0xbb8844, 1);
+		this.graphics.fillRect(x, y, w, h);
+		this.graphics.lineStyle(2, 0x000000, 1);
+		this.graphics.strokeRect(x, y, w, h);
 	}
 
 	startMovement(moveX, moveY, duration) {
